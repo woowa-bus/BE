b67b324685d9bd5d4a5d777a8fc6705bf374a1d8
jyt6640 <jyt6640@gmail.com>
2026-05-22 13:11:41 +0900

    chore: 프로젝트 초기 설정

---
+++ src/test/java/com/woowa/bus/BusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowa.bus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
